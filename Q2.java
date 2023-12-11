dbc373ba25a8ba3681ee823afac2f885ea33d2f8
Patryk Major <PATRYK.MAJOR.2024@mumail.ie>
2023-12-11 11:50:33 +0000

    lab ended, started Q2

--- Q2.java
+++ Q2.java
@@ -1,5 +1,55 @@
+import java.util.Scanner;
+
 public class Q2 {
     public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+        int month = sc.nextInt();
+        String dayz = "";
         
+
+        switch (month) {
+            case 1:
+                System.out.println("You selected "+day)
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            case 4:
+                
+                break;
+            case 5:
+                
+                break;
+            case 6:
+                
+                break;
+            case 7:
+                
+                break;
+            case 8:
+                
+                break;
+            case 9:
+                
+                break;
+            case 10:
+                
+                break;
+            case 11:
+                
+                break;
+            case 12:
+                
+                break;
+        
+            default:
+                break;
+        }
+
     }
 }
