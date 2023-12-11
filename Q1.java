9116960fa60aa1a4d9d02e028caf30d5bd12942c
Patryk Major <PATRYK.MAJOR.2024@mumail.ie>
2023-12-11 11:34:28 +0000

    added qeustion one

--- Q1.java
+++ Q1.java
@@ -1,7 +1,41 @@
+import java.util.Scanner;
+
 public class Q1{
     public static void main(String[] args){
 
-    System.out.println("gdgjdrg");
+        Scanner sc = new Scanner(System.in);
+
+        String input = "";
+        String stop = "q";
+        String square = "square";
+        String circle = "circle";
+        String rect = "rectangle";
+        int width = 0;
+        int length = 0;
+        int perim = 0;
+
+        while(true){
+
+            input = sc.nextLine();
+
+            if(input.equals(stop)){
+                break;
+            }
+            else{
+                if(input.equals(rect) || input.equals(square)){
+                    System.out.println("Enter Width and Length:");
+
+                    width = sc.nextInt();
+                    length = sc.nextInt();
+
+                    perim = (2*width) + (2* length);
+
+                    System.out.println("Perimiter: " + perim);
+
+                }
+            }
+
+        }
 
     }
 }
